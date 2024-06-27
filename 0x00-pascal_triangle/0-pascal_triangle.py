0d917edf1e0f93d084b13ae95be94a652a1cd422
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-06-27 18:38:13 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,15 +2,23 @@
 """
 pascal triangle
 """
+
 def pascal_triangle(n):
+    # Return an empty list if n is less than or equal to 0
     if n <= 0:
         return []
+    
+    # Initialize the triangle with the first row
     res = [[1]]
 
-    for i in range(n - 1):
+    # Build the Pascal's Triangle row by row
+    for i in range(1, n):
+        # Create the new row by adding 0 at both ends of the previous row
         temp = [0] + res[-1] + [0]
         row = []
+        # Sum adjacent elements to create the new row
         for j in range(len(res[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         res.append(row)
+    
     return res
