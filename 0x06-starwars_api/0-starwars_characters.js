ea2d096b5494b4d90cd41e8c81624653b1eb413b
brandonladen <brandonladen486@gmail.com>
2024-08-12 04:11:37 +0300

    shifted from axios to request method

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
 
-const axios = require('axios').default;
-movie_id = process.argv[2]
-async function getCharacters () {
-  try {
-    const people = await axios.get(`https://swapi-api.alx-tools.com/api/films/${movie_id}/`); //returns a list -> people.data['characters']
-    for(let i = 0; i < people.data['characters'].length; i++){
-        const character = await axios.get(people.data['characters'][i]);
-        console.log(character.data['name']);
-    };
-  } catch (error) {
+const request = require('request');
+
+const movie_id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
     console.error(error);
+    return;
   }
-}
-getCharacters()
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
